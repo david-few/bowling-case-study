80a59b915df54bf85dd2eabb5422c466bff6e206
unknown <david.few@noram.co.uk>
2020-06-15 10:25:38 +0100

    xxx

--- src/main/scala/code/Main.scala
+++ src/main/scala/code/Main.scala
@@ -1,5 +1,8 @@
 package code
 
-object Main extends App {
-  println("Hello world!")
+
+sealed trait Frame
+final case class Strike()
+final case class Spare(one: int,implicit two: int)
+final case class Regular(one: int, two: int)
 }
