c5408ad088bd4a28491fd01802b58ffc645ae8c2
unknown <david.few@noram.co.uk>
2020-06-20 21:36:51 +0100

    Retire EndState class in favour of BonusFrame class Complete and fix frameScore method (and rename from score) Add recursive allScore method Call allScore from calculate method Add test class and additional test cases

--- src/main/scala/code/Main.scala
+++ src/main/scala/code/Main.scala
@@ -2,24 +2,66 @@ package code
 
 sealed trait Frame
 final case class Strike() extends Frame
-final case class Spare(one: Int) extends Frame
-final case class Regular(one: Int, two: Int) extends Frame
+final case class Spare(rollOne: Int) extends Frame
+final case class Regular(rollOne: Int, rollTwo: Int) extends Frame
+final case class Bonus(rollOne: Int, rollTwo: Int) extends Frame
 
+sealed trait State
+final case class NormalState() extends State
+final case class SpareState() extends State
+final case class StrikeState() extends State
+final case class StrikeStrikeState() extends State
 
 object Main extends App {
 
-  val testcase1 = List(Strike(),Strike(),Strike(),Strike(),Strike(),Strike(),Strike(),Strike(),Strike(),Strike(),Strike(),Strike())
-  val testcase2 = List(Regular(9,0),Regular(9,0),Regular(9,0),Regular(9,0),Regular(9,0),Regular(9,0),Regular(9,0),Regular(9,0),Regular(9,0),Regular(9,0))
-  val testcase3 = List(Spare(5),Spare(5),Spare(5),Spare(5),Spare(5),Spare(5),Spare(5),Spare(5),Spare(5),Spare(5),Regular(5,0))
-
-  val result1: Int = 300
-  val result2: Int = 90
-  val result3: Int = 150
-
-  val tests (testcase :Frame) {
-    printf(result1 == calculate(testcase1))
-    printf(result2 == calculate(testcase2))
-    printf(result3 == calculate(testcase3))
-
+  def calculate(frames: List[Frame]): Int = {
+    return allScore(frames, NormalState(), 0, 0)
   }
+
+  def allScore(frames: List[Frame], state: State, i: Int, acc: Int) : Int = {
+    if (i == frames.size) return acc
+    println(i, acc)
+    val (score, newState) = frameScore(frames(i), state)
+    return allScore(frames, newState, i+1, acc+score)
+  }
+
+//  def nextState(frameIndex: Int, state: State): State =
+//    frameIndex match {
+//      case 0                        => NormalState()
+//      case i if 1 to 9 contains i   => state
+//      case _                        => EndState()
+//    }
+
+  def frameScore(frame: Frame, state: State): (Int, State) =
+    frame match {
+      case Bonus(rollOne, rollTwo) =>
+        state match {
+          case NormalState()        => (0, state)
+          case SpareState()         => (rollOne, state)
+          case StrikeStrikeState()  => (rollOne * 2 + rollTwo, state)
+          case StrikeState()        => (rollOne + rollTwo, state)
+        }
+      case Strike() =>
+        state match {
+          case NormalState()        => (10, StrikeState())
+          case SpareState()         => (20, StrikeState())
+          case StrikeStrikeState()  => (30, StrikeStrikeState())
+          case StrikeState()        => (20, StrikeStrikeState())
+        }
+      case Spare(rollOne) =>
+        state match {
+          case NormalState()        => (10, SpareState())
+          case SpareState()         => (rollOne + 10, SpareState())
+          case StrikeStrikeState()  => (rollOne + 20, SpareState())
+          case StrikeState()        => (20, SpareState())
+        }
+      case Regular(rollOne, rollTwo) =>
+        state match {
+          case NormalState()        => (rollOne + rollTwo, NormalState())
+          case SpareState()         => (rollOne * 2 + rollTwo, NormalState())
+          case StrikeStrikeState()  => ((rollOne * 3) + (rollTwo * 2), NormalState())
+          case StrikeState()        => ((rollOne + rollTwo) * 2, NormalState())
+        }
+    }
+
 }
--- src/test/scala/code/MainSpec.scala
+++ src/test/scala/code/MainSpec.scala
@@ -3,9 +3,93 @@ package code
 import org.scalatest._
 
 class MainSpec extends WordSpec with Matchers {
-  "true" should {
-    "equal false" in {
-      true shouldBe false
+  "calculate" should {
+    "correctly calculate all strikes" in {
+      val testcase1 = List(
+        Strike(),
+        Strike(),
+        Strike(),
+        Strike(),
+        Strike(),
+        Strike(),
+        Strike(),
+        Strike(),
+        Strike(),
+        Strike(),
+        Bonus(10,10)
+      )
+
+      Main.calculate(testcase1) shouldBe 300
+    }
+
+    "correctly calculate all regular" in {
+      val testcase2 = List(
+        Regular(9, 0),
+        Regular(9, 0),
+        Regular(9, 0),
+        Regular(9, 0),
+        Regular(9, 0),
+        Regular(9, 0),
+        Regular(9, 0),
+        Regular(9, 0),
+        Regular(9, 0),
+        Regular(9, 0)
+      )
+      Main.calculate(testcase2) shouldBe 90
+    }
+
+    "correctly calculate all spares" in {
+      val testcase3 = List(
+        Spare(5),
+        Spare(5),
+        Spare(5),
+        Spare(5),
+        Spare(5),
+        Spare(5),
+        Spare(5),
+        Spare(5),
+        Spare(5),
+        Spare(5),
+        Bonus(5, 0)
+      )
+
+      Main.calculate(testcase3) shouldBe 150
+    }
+
+    "correctly calculate mix 1" in {
+      val testcase4 = List(
+        Spare(5),
+        Strike(),
+        Spare(5),
+        Strike(),
+        Strike(),
+        Spare(3),
+        Regular(2,7),
+        Regular(9,0),
+        Spare(1),
+        Spare(2),
+        Bonus(9, 0)
+      )
+
+      Main.calculate(testcase4) shouldBe 164
+    }
+
+    "correctly calculate mix 2" in {
+      val testcase4 = List(
+        Spare(5),
+        Strike(),
+        Spare(5),
+        Regular(4,5),
+        Strike(),
+        Spare(3),
+        Regular(2,7),
+        Regular(9,0),
+        Spare(1),
+        Strike(),
+        Bonus(10, 10)
+      )
+
+      Main.calculate(testcase4) shouldBe 163
     }
   }
 }
